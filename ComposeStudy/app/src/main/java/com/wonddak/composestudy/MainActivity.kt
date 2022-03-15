70ae395d376e585cb5c2aa4efaea1239f8baa256
jmseb3 <jmseb3@naver.com>
2022-03-15 22:16:49 +0900

    [Android] Jetpack Compose 알아보기 - (2)
    [Android] Jetpack Compose 알아보기 - (2)

--- ComposeStudy/app/src/main/java/com/wonddak/composestudy/MainActivity.kt
+++ ComposeStudy/app/src/main/java/com/wonddak/composestudy/MainActivity.kt
@@ -3,13 +3,18 @@ package com.wonddak.composestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wonddak.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,5 +44,32 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeStudyTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTwoText() {
+    TwoText(text1 = "안녕하세요", text2 = "저는 차도리입니다.")
+}
+
+@Composable
+fun TwoText(text1: String, text2: String) {
+    val heightDp = 100.dp
+
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "",
+            modifier = Modifier.height(heightDp).width(50.dp)
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.height(heightDp)
+        ) {
+            Text(text = text1)
+            Text(text = text2)
+        }
     }
 }
